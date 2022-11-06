ca3df95ed3aefcddbd80f888f98d4ac2542d1192
Kalejaiye Tolulope <tolulopekay56@gmail.com>
2022-11-06 13:13:54 -0800

    deadline

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import { Helmet } from 'react-helmet';
 import './App.css'
 import Counter from "./Components/Counter"
 import {Route, Routes} from "react-router-dom"
@@ -11,6 +11,10 @@ import { ErrorBoundary } from './Components/ErrorBoundary'
 function App() {
   return(
     <div className ="container">
+      <Helmet>
+        <title>React Counter App</title>
+        <meta name="description" content="My attempt at building a react counter app, this is my entry for second semester exam for AltSchool Frontend Enginnering"/>
+      </Helmet>
     <ErrorBoundary>
     <h1 className="app-header">Tolulope's React Counter App</h1>
      <Counter/>
