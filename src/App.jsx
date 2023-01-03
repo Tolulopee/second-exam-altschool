fef27b2773f6ce3ff6abc148b0a12eacefd5f0b2
Kalejaiye Tolulope <tolulopekay56@gmail.com>
2023-01-03 13:44:02 +0000

    Update App.jsx and ErrorBoundary.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
-import { Helmet } from 'react-helmet';
-import './App.css'
-import Counter from "./Components/Counter"
-import {Route, Routes} from "react-router-dom"
-import PageNotFound from "./Components/PageNotFound"
-import { ErrorBoundary } from './Components/ErrorBoundary'
-
-
-
+import { Helmet } from "react-helmet";
+import "./App.css";
+import Counter from "./Components/Counter";
+import { Route, Routes } from "react-router-dom";
+import PageNotFound from "./Components/PageNotFound";
+import ErrorBoundary from "./Components/ErrorBoundary";
 
 function App() {
-  return(
-    <div className ="container">
+  return (
+    <div className="container">
       <Helmet>
         <title>React Counter App</title>
-        <meta name="description" content="My attempt at building a react counter app, this is my entry for second semester exam for AltSchool Frontend Enginnering"/>
+        <meta
+          name="description"
+          content="My attempt at building a react counter app, this is my entry for second semester exam for AltSchool Frontend Enginnering"
+        />
       </Helmet>
-    <ErrorBoundary>
-    <h1 className="app-header">Tolulope's React Counter App</h1>
-     <Counter/>
-     <Routes>
-      <Route component={<PageNotFound/>}></Route>
-     </Routes>
-     </ErrorBoundary>
+
+      <h1 className="app-header">Tolulope's React Counter App</h1>
+
+      <Counter />
+      <Routes>
+        <Route component={<PageNotFound />}></Route>
+        <Route component={<ErrorBoundary />}>ErrorBoundary Page</Route>
+      </Routes>
     </div>
-  )
-
-  
-    
-
+  );
 }
 
-export default App
+export default App;
