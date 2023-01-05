01aedb53e7121cdf1ccd7c6da6a631ccb511f7e7
Kalejaiye Tolulope <tolulopekay56@gmail.com>
2023-01-05 21:39:15 +0000

    Update 3 files

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,43 +1,39 @@
+//import {useReducer} from "react";
+import useCustomCounter from "./useCustomCounter";
 
-import {useReducer} from "react";
+//const initialState = {count:0}
 
-const initialState = {count:0}
+//function reducer (state, action){
+//switch (action.type){
+//case "increment":
+// return {count:state.count + 1}
+// case "decrement":
+// return {count:state.count - 1}
+// case "resetvalue":
+// return initialState
+//The setvalue here is still set to just + 5, will try to fix it
+// case "setvalue":
+//    return {count:state.count + 5};
+// default:
+// throw new Error();
+// }
+// }
 
-function reducer (state, action){
-    switch (action.type){
-        case "increment":
-            return {count:state.count + 1}
-            case "decrement":
-                return {count:state.count - 1}
-                case "resetvalue":
-                    return initialState
-                    //The setvalue here is still set to just + 5, will try to fix it
-                    case "setvalue":
-                        return {count:state.count + 5};
-                    default:
-                        throw new Error();
-    }
-}
-
-
-
-
-
-
-function Counter(){
-    const [state, dispatch] = useReducer(reducer, initialState);
-    return(
-        <div>
-            <div className = "display-part">
-            {state.count}
-            </div>
-            <button onClick={()=>dispatch({type:"increment"})
-            }>Increment</button>
-            <button onClick={()=>dispatch({type:"decrement"})}>Decrement</button>
-            <button onClick={()=>dispatch({type:"resetvalue"})}>Reset</button>
-            <button onClick={()=>dispatch({type:"setvalue"})}>SetValue</button>
-        </div>
-    )
+function Counter() {
+  const [count, Increment, Decrement, resetValue, setValue] = useCustomCounter(
+    0,
+    1
+  );
+  //const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <div>
+      <div className="display-part">{count}</div>
+      <button onClick={Increment}>Increment</button>
+      <button onClick={Decrement}>Decrement</button>
+      <button onClick={resetValue}>Reset</button>
+      <button onClick={setValue}>SetValue</button>
+    </div>
+  );
 }
 
 export default Counter;
---
+++ src/Components/useCustomCounter.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+function useCustomCounter(val, step) {
+  const [count, setCount] = useState(val);
+
+  function Increment() {
+    setCount(count + step);
+  }
+
+  function Decrement() {
+    setCount(count - step);
+  }
+
+  function resetValue() {
+    setCount(0);
+  }
+
+  function setValue() {
+    setCount(count = val);
+  }
+
+  return [count, Increment, Decrement, resetValue, setValue];
+}
+
+export default useCustomCounter;
